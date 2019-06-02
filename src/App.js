913565b2d2a5680677decc97af05c20db72d8300
JeffOgah <jeff.ogah@gmail.com>
2019-06-02 20:53:59 +0100

    Simple clock app made with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clock: true
+    }
+    this.displayClock = this.displayClock.bind(this);
+  }
+  displayClock() {
+    this.setState({
+      clock: !this.state.clock
+    })
+    console.log(this.state.clock)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Clock clickHandler={this.displayClock} clockState={this.state.clock}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Clock.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    };
+    this.handleClick = this.handleClick.bind(this)
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  tick() {
+      this.setState({
+          time: new Date().toLocaleString()
+      })
+  }
+  componentWillMount(){
+      clearInterval(this.intervalID)
+  }
+  handleClick() {
+    this.props.clickHandler();
+  }
+  render() {
+    return (
+      <div className="">
+        <button className="btn btn-primary" onClick={this.handleClick}>{!this.props.clockState? "Show Clock" : "Hide Clock"}</button>
+        {this.props.clockState? (<p>The time is {this.state.time}</p>) : ""}
+      </div>
+    );
+  }
+}
+
+export default Clock;
